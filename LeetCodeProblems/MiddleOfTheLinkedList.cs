5dc982b885c55fb04f0703691c14099b3049b54e
Эльдар Букеев <bukeevel@gmail.com>
2022-10-07 14:28:35 +0300

    Solved problem #876

---
+++ LeetCodeProblems/MiddleOfTheLinkedList.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class MiddleOfTheLinkedList
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        public ListNode MiddleNode(ListNode head)
+        {
+            var currentNode = head;
+            int indexOfLast = 1;
+            while(currentNode.next != null)
+            {
+                currentNode = currentNode.next;
+                indexOfLast++;
+            }
+            int middleIndex = indexOfLast / 2 + 1;
+            currentNode = head;
+            indexOfLast = 1;
+            while(indexOfLast != middleIndex)
+            {
+                currentNode = currentNode.next;
+                indexOfLast++;
+            }
+            return currentNode;
+        }
+    }
+}
---
+++ TestLeetCodeProblems/MiddleOfLinkedListTest.cs
@@ -0,0 +1,28 @@
+﻿using LeetCodeProblems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCodeProblems.MiddleOfTheLinkedList;
+
+namespace TestLeetCodeProblems
+{
+    public class MiddleOfLinkedListTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            var algo = new MiddleOfTheLinkedList();
+            var node5 = new ListNode(5, null);
+            var node4 = new ListNode(4, node5);
+            var node3 = new ListNode(3, node4);
+            var node2= new ListNode(2, node3);
+            var node1= new ListNode(1, node2);
+            var actual = algo.MiddleNode(node1);
+            var expected = node3;
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
