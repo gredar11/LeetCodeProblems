cf1966aed47e1f2b7831f781227596b91e5e0d55
Эльдар Букеев <bukeevel@gmail.com>
2022-10-14 13:53:16 +0300

    Solved Remove duplicates from sorted linked list problem

---
+++ LeetCodeProblems/RemoveDuplicatesFromSortedLinkedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCodeProblems.LinkedListCycle;
+
+namespace LeetCodeProblems
+{
+    public class RemoveDuplicatesFromSortedLinkedList
+    {
+        // source: https://leetcode.com/problems/remove-duplicates-from-sorted-list/submissions/
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            var currentNode = head?.next;
+            var previous = head;
+            while(currentNode != null)
+            {
+                var next = currentNode.next;
+                if (currentNode.val == previous.val)
+                {
+                    previous.next = next;
+                }
+                else
+                {
+                    previous = currentNode;
+                }
+                currentNode = next;
+            }
+            return head;
+        }
+    }
+}
---
+++ TestLeetCodeProblems/RemoveDuplicatesTest.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeProblems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCodeProblems.LinkedListCycle;
+
+namespace TestLeetCodeProblems
+{
+    public class RemoveDuplicatesTest
+    {
+        [Fact]
+        public void TestMethod()
+        {
+            var helper = new RemoveDuplicatesFromSortedLinkedList();
+            
+            ListNode n3 = new ListNode(1);
+            ListNode n2 = new ListNode(1) { next = n3 };
+            ListNode n1 = new ListNode(1) { next = n2};
+            var tail = helper.DeleteDuplicates(/*n1*/n1);
+            Console.WriteLine();
+        }
+    }
+}
