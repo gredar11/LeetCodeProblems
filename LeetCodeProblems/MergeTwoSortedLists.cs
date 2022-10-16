1780e97009dd38dae46a76d8d5050758d64b149b
Эльдар Букеев <bukeevel@gmail.com>
2022-10-16 17:34:18 +0300

    MergeList problem

---
+++ LeetCodeProblems/MergeTwoSortedLists.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCodeProblems.LinkedListCycle;
+
+namespace LeetCodeProblems
+{
+    public class MergeTwoSortedLists
+    {
+        public ListNode ConvertArrayToListNode(int[] array)
+        {
+            ListNode node = new ListNode(array[^1]);
+            for (int i = array.Length - 2 ; i >= 0; i--)
+            {
+                var temp = new ListNode(array[i]) { next = node };
+                node = temp;
+            }
+            return node;
+        }
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode firstPointer = list1;
+            ListNode secondPointer = list2;
+            if (list1 == null && list2 == null)
+                return null;
+            if (list1 != null && list2 == null)
+                return list1;
+            if (list1 == null && list2 != null)
+                return list2;
+            while (firstPointer != null && secondPointer != null)
+            {
+                if (firstPointer.val < secondPointer.val)
+                {
+                    while (firstPointer.next != null && firstPointer.next.val < secondPointer.val)
+                    {
+                        firstPointer = firstPointer.next;
+                        while (firstPointer.next != null && firstPointer.next.val == firstPointer.val)
+                            firstPointer = firstPointer.next;
+                    }
+                    var temp = firstPointer.next;
+                    firstPointer.next = secondPointer;
+                    var temp2 = secondPointer.next;
+                    secondPointer.next = temp;
+                    firstPointer = firstPointer.next;
+                    secondPointer = temp2;
+                }
+                else
+                {
+                    while (secondPointer.next != null && secondPointer.next.val < firstPointer.val)
+                    {
+                        secondPointer = secondPointer.next;
+                        while (secondPointer.next != null && secondPointer.next.val == secondPointer.val)
+                            secondPointer = secondPointer.next;
+                    }
+                    var temp = secondPointer.next;
+                    secondPointer.next = firstPointer;
+                    var temp2 = firstPointer.next;
+                    firstPointer.next = temp;
+                    firstPointer = temp2;
+                    secondPointer = secondPointer.next;
+                }
+            }
+
+            return list1.val < list2.val ? list1 : list2;
+        }
+    }
+
+}
---
+++ TestLeetCodeProblems/MergeTwoLinkedListTest.cs
@@ -0,0 +1,25 @@
+﻿using LeetCodeProblems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCodeProblems.LinkedListCycle;
+
+namespace TestLeetCodeProblems
+{
+    public class MergeTwoLinkedListTest
+    {
+        [Fact]
+        public void TestMerges()
+        {
+            
+            var helper = new MergeTwoSortedLists();
+            var node1 = helper.ConvertArrayToListNode(new int[] { -10, -9, -6, -4, 1, 9, 9 });
+            var node2 = helper.ConvertArrayToListNode(new int[] { -5, -3, 0, 7, 8, 8 });
+
+            var res = helper.MergeTwoLists(node1, node2);
+            Console.WriteLine();
+        }
+    }
+}
