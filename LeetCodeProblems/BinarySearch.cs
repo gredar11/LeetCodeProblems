122c2e8c3c12f3f011acc0ccde58e47f9e3dbb32
Эльдар Букеев <bukeevel@gmail.com>
2022-10-18 11:03:09 +0300

    solved binary search and first greater letter problems

---
+++ LeetCodeProblems/BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class BinarySearch
+    {
+        public int Search(int[] nums, int target)
+        {
+            int l = 0;
+            int h =nums.Length -1;
+            while (l < h)
+            {
+                int mid = l + (h - l) / 2;
+                if(nums[mid] == target)
+                    return mid;
+                if(nums[mid] > target)
+                    h = mid;
+                if (nums[mid] < target)
+                    l = mid + 1;
+            }
+            if (nums[h] == target)
+                return h;
+            return -1;
+        }
+    }
+}
---
+++ LeetCodeProblems/FindFirstGreaterChar.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems
+{
+    public class FindFirstGreaterChar
+    {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            int upper = letters.Length - 1;
+            int lower = 0;
+            int res = 0;
+            while (upper >= lower)
+            {
+                int mid = lower + (upper - lower) / 2;
+                if (letters[mid] > target && lower == upper)
+                {
+                    res = mid;
+                    break;
+                }
+                if (letters[mid] > target)
+                {
+                    upper = mid;
+                    res = mid;
+                }
+                if (letters[mid] <= target)
+                {
+                    lower = mid + 1;
+                }
+
+            }
+            return letters[res];
+        }
+    }
+}
