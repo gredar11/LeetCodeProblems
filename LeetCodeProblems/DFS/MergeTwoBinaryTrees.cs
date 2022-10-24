61c4a972429e1eb40b0b3e9501f8c90281fef530
Эльдар Букеев <bukeevel@gmail.com>
2022-10-24 10:33:43 +0300

    solved "merge two BT" problem

---
+++ LeetCodeProblems/DFS/MergeTwoBinaryTrees.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCodeProblems.DFS.InvertBinaryTree;
+
+namespace LeetCodeProblems.DFS
+{
+    public class MergeTwoBinaryTrees
+    {
+        public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == null || root2 == null)
+            {
+                return root1 ?? root2;
+            }
+            Queue<TreeNode> queue1 = new Queue<TreeNode>();
+            Queue<TreeNode> queue2 = new Queue<TreeNode>();
+            queue1.Enqueue(root1);
+            queue2.Enqueue(root2);
+            TreeNode resultNode = null;
+            while (queue1.Count > 0 && queue2.Count > 0)
+            {
+                TreeNode node1 = null;
+                TreeNode node2 = null;
+                node1 = queue1.Dequeue();
+                node2 = queue2.Dequeue();
+                resultNode = node1 ?? node2;
+                if (node1 != null && node2 != null)
+                {
+                    queue1.Enqueue(node1.left);
+                    queue2.Enqueue(node2.left);
+                    queue1.Enqueue(node1.right);
+                    queue2.Enqueue(node2.right);
+                    resultNode.val = node1.val + node2.val;
+                    resultNode.left = node1.left ?? node2.left;
+                    resultNode.right = node1.right ?? node2.right;
+                    continue;
+                }
+                if (node1 == null && node2 == null)
+                {
+                    continue;
+                }
+                if (node1 == null)
+                {
+                    resultNode.left = node2.left;
+                    queue1.Enqueue(null);
+                    queue2.Enqueue(resultNode.left);
+                    resultNode.right = node2.right;
+                    queue1.Enqueue(null);
+                    queue2.Enqueue(resultNode.right);
+                    continue;
+                }
+                if (node2 == null)
+                {
+                    resultNode.left = node1.left;
+                    queue1.Enqueue(resultNode.left);
+                    queue2.Enqueue(null);
+                    resultNode.right = node1.right;
+                    queue1.Enqueue(resultNode.right);
+                    queue2.Enqueue(null);
+                    continue;
+                }
+            }
+            return root1 ?? root2;
+        }
+    }
+}
--- TestLeetCodeProblems/BinaryTree/InvertBinaryTreeTest.cs
+++ TestLeetCodeProblems/BinaryTree/InvertBinaryTreeTest.cs
@@ -38,5 +38,20 @@ namespace TestLeetCodeProblems.BinaryTree
             var invertedTree = helper.HasPathSum(treeNode1_1,7);
             Console.WriteLine();
         }
+        [Fact]
+        public void TestMergeTwoTrees()
+        {
+            TreeNode treeNode1_4 = new TreeNode(5);
+            TreeNode treeNode1_3 = new TreeNode(2);
+            TreeNode treeNode1_2 = new TreeNode(3,treeNode1_4);
+            TreeNode treeNode1_1 = new TreeNode(1,treeNode1_2, treeNode1_3);
+            TreeNode treeNode2_5 = new TreeNode(7);
+            TreeNode treeNode2_4 = new TreeNode(4);
+            TreeNode treeNode2_3 = new TreeNode(3,null, treeNode2_5);
+            TreeNode treeNode2_2 = new TreeNode(1, null, treeNode2_4);
+            TreeNode treeNode2_1 = new TreeNode(2,treeNode2_2, treeNode2_3);
+            var helper = new MergeTwoBinaryTrees();
+            var res = helper.MergeTrees(treeNode1_1, treeNode2_1);
+        }
     }
 }
