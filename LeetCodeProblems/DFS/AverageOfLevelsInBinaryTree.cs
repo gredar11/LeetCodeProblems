bdc8459214d208aada7e8257f7fb5b25a31b6a02
Эльдар Букеев <bukeevel@gmail.com>
2022-10-24 11:35:39 +0300

    Solved problems: -average of levels bt -min and max depth of bt

---
+++ LeetCodeProblems/DFS/AverageOfLevelsInBinaryTree.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCodeProblems.DFS.InvertBinaryTree;
+
+namespace LeetCodeProblems.DFS
+{
+    public class AverageOfLevelsInBinaryTree
+    {
+        public IList<double> AverageOfLevels(TreeNode root)
+        {
+            List<double> averageLevels = new List<double>();
+            averageLevels.Add(root.val);
+            List<TreeNode> currentLayer = new List<TreeNode>();
+            currentLayer.Add(root);
+            while (currentLayer.Count > 0)
+            {
+                int countOfNotNull = 0;
+                double summOfLayer = 0.0;
+                List<TreeNode> nextLayer = new List<TreeNode>();
+
+                foreach (var node in currentLayer)
+                {
+                    if (node.left is not null)
+                    {
+                        countOfNotNull++;
+                        summOfLayer += node.left.val;
+                        nextLayer.Add(node.left);
+                    }
+                    if (node.right is not null)
+                    {
+                        countOfNotNull++;
+                        summOfLayer += node.right.val;
+                        nextLayer.Add(node.right);
+
+                    }
+                }
+                if (countOfNotNull == 0)
+                    break;
+                averageLevels.Add(summOfLayer/countOfNotNull);
+                currentLayer = nextLayer;
+            }
+            return averageLevels;
+        }
+    }
+}
---
+++ LeetCodeProblems/DFS/MinAndMaxDepthOfBinaryTree.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCodeProblems.DFS.InvertBinaryTree;
+
+namespace LeetCodeProblems.DFS
+{
+    public class MinAndMaxDepthOfBinaryTree
+    {
+        public int MinDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            List<TreeNode> currentLayer = new List<TreeNode>();
+            currentLayer.Add(root);
+            int depth = 1;
+            while (currentLayer.Count > 0)
+            {
+                List<TreeNode> nextLayer = new List<TreeNode>();
+                foreach (var node in currentLayer)
+                {
+                    if (node.left == null && node.right == null)
+                    {
+                        return depth;
+                    }
+                    if (node.left is not null)
+                    {
+                        nextLayer.Add(node.left);
+                    }
+                    if (node.right is not null)
+                    {
+                        nextLayer.Add(node.right);
+                    }
+                }
+                depth++;
+                currentLayer = nextLayer;
+            }
+            return depth;
+        }
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+            List<TreeNode> currentLayer = new List<TreeNode>();
+            currentLayer.Add(root);
+            int depth = 1;
+            while (currentLayer.Count > 0)
+            {
+                List<TreeNode> nextLayer = new List<TreeNode>();
+                foreach (var node in currentLayer)
+                {
+                    if (node.left is not null)
+                    {
+                        nextLayer.Add(node.left);
+                    }
+                    if (node.right is not null)
+                    {
+                        nextLayer.Add(node.right);
+                    }
+                }
+                if (nextLayer.Count == 0)
+                    return depth;
+                depth++;
+                currentLayer = nextLayer;
+            }
+            return depth;
+        }
+    }
+}
